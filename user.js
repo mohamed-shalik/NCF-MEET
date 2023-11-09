0e3b14867ce1d48b0d406b12f26b88969b5904e4
Md shalik <147500650+mohamed-shalik@users.noreply.github.com>
2023-11-09 09:49:15 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,40 @@
+function normalizeAddress(address) {
+    // Check if the address is a valid Ethereum address
+    if (!/^0x[0-9a-fA-F]{40}$/.test(address)) {
+        throw new Error('Invalid Ethereum address');
+    }
+
+    // Convert the address to lowercase
+    return address.toLowerCase();
+}
+        
+        document.getElementById("goBackButton").addEventListener("click", function() {
+            window.location.href = "index.html";
+        });
+
+        document.getElementById("connectWalletButton").addEventListener("click", async function() {
+            if (typeof ethereum !== 'undefined') {
+                try {
+                    // Request permission to connect to MetaMask
+                    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+                    const userAddress = normalizeAddress(accounts[0]);
+
+                    // Initialize Firebase with your configuration
+                    const firebaseConfig = {
+            apiKey: "AIzaSyAeKo2LsvFJvz_5cFYBwJQWqGGDrd73zaA",
+            authDomain: "ncf-auth.firebaseapp.com",
+            databaseURL: "https://ncf-auth-default-rtdb.firebaseio.com",
+            projectId: "ncf-auth",
+            storageBucket: "ncf-auth.appspot.com",
+            messagingSenderId: "507271869782",
+            appId: "1:507271869782:web:995e3299d0924b3d18d2f7",
+            measurementId: "G-LG3149FZHG"
+        };
+        window.location.href = "userauth.html";
+                } catch (error) {
+                    console.error(error);
+                }
+            } else {
+                alert('Please install MetaMask to use this feature.');
+            }
+        });
